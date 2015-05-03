21260b0a3500d435ac92a86e134b8a60bbd9c086
Galina Schmidt <gs027@hdm-stuttgart.de>
2015-05-03 17:38:50 +0200

    Baugruppen Objekt wurde angelegt und get/set methode deklariert.

--- hdm-sms/src/de/hdm/gruppe1/shared/bo/Baugruppe.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/bo/Baugruppe.java
@@ -1,5 +1,19 @@
 package de.hdm.gruppe1.shared.bo;
 
-public class Baugruppe {
-
+public class Baugruppe extends Element{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * die Baugruppe werden in Stücklisten verwendet, dadruch werden
+	 * in die Klassen Stückliste angelegt und gesetzt.
+	 **/
+	Stueckliste stueckliste = "";
+	
+	public getStueckliste(){
+		return this.stueckliste;
+	}
+	public void setStueckliste(Stueckliste stueckliste){
+	this.stueckliste = stueckliste;
+	}
 }
