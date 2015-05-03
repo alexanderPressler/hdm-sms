9ed758ed9a24e9e6184588f2bcc281853158b2a5
Galina Schmidt <gs027@hdm-stuttgart.de>
2015-05-03 22:17:18 +0200

    Asyncronous einrichten als Gegenstück zu Interface Sms

--- hdm-sms/src/de/hdm/gruppe1/shared/SmsAsync.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/SmsAsync.java
@@ -1,33 +1,51 @@
 package de.hdm.gruppe1.shared;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+import de.hdm.gruppe1.shared.bo.Baugruppe;
 import de.hdm.gruppe1.shared.bo.Bauteil;
+import de.hdm.gruppe1.shared.bo.Element;
 
 /**
- * Das asynchrone Gegenstück des Interface {@link BankAdministration}. Es wird
+ * Das asynchrone Gegenstueck des Interface {@link Sms}. Es wird
  * semiautomatisch durch das Google Plugin erstellt und gepflegt. Daher erfolgt
- * hier keine weitere Dokumentation. Für weitere Informationen siehe das
- * synchrone Interface {@link BankAdministration}.
+ * hier keine weitere Dokumentation. Fuer weitere Informationen siehe das
+ * synchrone Interface {@link Sms}.
  * 
- * @author thies
+ * @author thies& Schmidt
  */
 public interface SmsAsync {
 	
 	void createBauteil(String bauteilBeschreibung,
 			String materialBeschreibung, AsyncCallback<Bauteil> callback);
 
-	void save(Bauteil b, AsyncCallback<Void> callback);
+	void save(Bauteil b, AsyncCallback<Bauteil> callback);
 
-	void delete(Bauteil b, AsyncCallback<Void> callback);
+	void delete(Bauteil b, AsyncCallback<Bauteil> callback);
 
-	void init(AsyncCallback<Void> callback);
+	void init(AsyncCallback<Bauteil> callback);
 
 	void getAllBauteile(AsyncCallback<Vector<Bauteil>> callback);
 	
 	
+	
+	
+	
+	void createBaugruppe(String name, ArrayList<Element> element,
+			AsyncCallback<Baugruppe> callback);
+	
+	void editBaugruppe(Baugruppe baugruppe, AsyncCallback<Baugruppe> callback);
+	
+	void deleteBaugruppe(Baugruppe baugruppe, AsyncCallback<Baugruppe> callback);
 
+	void getBaugruppeByName(String name, AsyncCallback<Baugruppe> callback);
 
+	void getBaugruppeById(int id, AsyncCallback<Baugruppe> callback);
+
+	void getAllBaugruppen(ArrayList<Baugruppe> baugruppe,
+			AsyncCallback<Baugruppe> callback);
+	
 }
