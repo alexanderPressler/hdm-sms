95f2f1975e2eea55e6a6008839f70117128d6cca
Andreas Herrmann <ah157@hdm-stuttgart.de>
2015-05-13 20:31:05 +0200

    Klasse ElementPaar erstellt

---
+++ hdm-sms/src/de/hdm/gruppe1/shared/bo/ElementPaar.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package de.hdm.gruppe1.shared.bo;
+
+/**
+ * @author Andreas Herrmann
+ *
+ */
+public class ElementPaar {
+	private int anzahl=0;
+	private Element element = null;
+	
+	public int getAnzahl() {
+		return anzahl;
+	}
+	public void setAnzahl(int anzahl) {
+		this.anzahl = anzahl;
+	}
+	public Element getElement() {
+		return element;
+	}
+	public void setElement(Element element) {
+		this.element = element;
+	}
+}
