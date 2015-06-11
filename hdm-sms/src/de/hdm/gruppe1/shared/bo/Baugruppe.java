ecc3e63809cc66489ea6f6adb0c13c35f1c95d7a
Andreas Herrmann <ah157@hdm-stuttgart.de>
2015-06-11 14:58:55 +0200

    Baugruppenklasse hinzugefügt

---
+++ hdm-sms/src/de/hdm/gruppe1/shared/bo/Baugruppe.java
@@ -0,0 +1,13 @@
+package de.hdm.gruppe1.shared.bo;
+
+public class Baugruppe extends Element {
+	private Stueckliste stueckliste;
+
+	public Stueckliste getStueckliste() {
+		return stueckliste;
+	}
+
+	public void setStueckliste(Stueckliste stueckliste) {
+		this.stueckliste = stueckliste;
+	}
+}
