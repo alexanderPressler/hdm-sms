527cdf158642c73f432811fcac4ef92ffb94e122
Galina Schmidt <gs027@hdm-stuttgart.de>
2015-06-17 10:52:04 +0200

    RPC Klassen für SMSReport erzeugnen und mit LoopPrevention ergänzen

---
+++ hdm-sms/src/de/hdm/gruppe1/shared/SmsReportAsync.java
@@ -0,0 +1,17 @@
+package de.hdm.gruppe1.shared;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.hdm.gruppe1.shared.report.MaterialBedarfReport;
+import de.hdm.gruppe1.shared.report.StrukturStuecklisteReport;
+
+public class SmsReportAsync {
+
+	void createMaterialBedarfReport(AsyncCallback<MaterialBedarfReport> callback){}
+
+	void init(AsyncCallback<Void> callback){}
+
+	void createStrukturStuecklisteReport(
+			AsyncCallback<StrukturStuecklisteReport> callback){}
+
+}
