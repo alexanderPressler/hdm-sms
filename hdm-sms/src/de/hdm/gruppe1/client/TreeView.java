edbec8247045c6352d2b2f4b81d2cd365cf5f706
Mario Theiler <Mario Theiler mt048@hdm-stuttgart.de>
2015-06-23 12:49:25 +0200

    StuecklisteGeneralView um RadioButton erweitert, neue Klasse TreeView

---
+++ hdm-sms/src/de/hdm/gruppe1/client/TreeView.java
@@ -0,0 +1,13 @@
+package de.hdm.gruppe1.client;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import de.hdm.gruppe1.shared.bo.Stueckliste;
+
+public class TreeView extends VerticalPanel {
+	
+	public TreeView(Stueckliste treeViewStueckliste) {
+		Window.alert("Hier anzeigen.");
+	}
+
+}
