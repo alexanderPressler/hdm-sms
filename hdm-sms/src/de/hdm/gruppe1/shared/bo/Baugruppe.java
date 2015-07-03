53b3bf30b257c20b3fe5363b0b5aabe2b784d48c
Galina Schmidt <gs027@hdm-stuttgart.de>
2015-07-03 10:10:45 +0200

    Baugruppen kommentieren

--- hdm-sms/src/de/hdm/gruppe1/shared/bo/Baugruppe.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/bo/Baugruppe.java
@@ -1,17 +1,45 @@
 package de.hdm.gruppe1.shared.bo;
 
-import com.google.gwt.user.client.ui.ListBox;
+
+
+/**
+ * Realisierung einer exemplarischen Baugruppe. 
+ * in diesem Fall wird die Baugruppe durch eine Stueckliste erzeugt.
+ * Stücklisten stellen die Struktur von Baugruppen dar. In der Stuecklisten Klasse 
+ * {@link } werden die Baugrupppen in Form von eines BaugruppenPaar-Vektors erzeugt. 
+ * Da eine Baugruppe aus weiteren Komponenten besteht wie Baugruppen oder auch Bauteilen
+ * wird es in unseren Fall als in der Baugruppe Klasse doe Sueckliste als Struktur
+ * erzeugt.
+ * 
+ * In eine Aggregation werden die Ganzes-Teile-Beziehungen abgebildet, was in unseren 
+ * Fall Baugruppen als ganzens betrachtet werden und Klasse Element als Teile davon.
+ * Dabei bestehen Baugruppen aus Stücklisten, die ebenfalls von Element erben.
+ * 
+ * 
+ * @author Schmidt
+ * 
+ */
 
 public class Baugruppe extends Element {
+	
 
 	private static final long serialVersionUID = 1L;
-	
+	 
+	/**
+	 * Stueckliste deklarieren für die Baugruppe
+	 */
 	private Stueckliste stueckliste;
+	
+	/**
+	   * Auslesen der stueckliste.
+	   */
 
 	public Stueckliste getStueckliste() {
 		return this.stueckliste;
 	}
-
+	 /**
+	   * Setzen der stueckliste.
+	   */
 	public void setStueckliste(Stueckliste stueckliste) {
 		this.stueckliste = stueckliste;
 	}
