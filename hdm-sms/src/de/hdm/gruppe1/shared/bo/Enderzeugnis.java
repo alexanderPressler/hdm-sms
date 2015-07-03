23501a5561d2a5dd3cccd44597f6db5e2c7065a8
Galina Schmidt <gs027@hdm-stuttgart.de>
2015-07-03 12:30:01 +0200

    Enderzeugnis kommentieren und formatieren

--- hdm-sms/src/de/hdm/gruppe1/shared/bo/Enderzeugnis.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/bo/Enderzeugnis.java
@@ -1,13 +1,37 @@
-
 package de.hdm.gruppe1.shared.bo;
 
+/**
+ * Ein Enderzeugnis ist eine spezielle Baugruppe. Diese Klasse erbt über die
+ * Element-Klasse von der Baugruppe die Objekte.
+ * 
+ * @author Schmidt
+ * 
+ */
 public class Enderzeugnis extends Element {
 
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * neues BaugruppenObjekt erzeugen
+	 */
 	Baugruppe baugruppe = new Baugruppe();
+
+	/**
+	 * 
+	 * Auslesen der baugruppe.
+	 * 
+	 * @return baugruppe
+	 */
+
 	public Baugruppe getBaugruppe() {
 		return baugruppe;
 	}
+
+	/**
+	 * Setzen der stueckliste.
+	 * 
+	 * @param baugruppe
+	 */
 	public void setBaugruppe(Baugruppe baugruppe) {
 		this.baugruppe = baugruppe;
 	}
