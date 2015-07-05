16e06979f2493f35148415f26f90fe2020a52474
Galina Schmidt <gs027@hdm-stuttgart.de>
2015-07-05 22:26:21 +0200

    Javadoc ergänzen final

--- hdm-sms/src/de/hdm/gruppe1/shared/bo/BusinessObject.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/bo/BusinessObject.java
@@ -1,10 +1,6 @@
 package de.hdm.gruppe1.shared.bo;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
-import java.util.Date;
-
-import com.google.appengine.api.users.User;
 
 /**
  * <p>
@@ -21,7 +17,7 @@ import com.google.appengine.api.users.User;
  * textuelle Notation in JSON (siehe http://www.json.org/) kodiert.
  * </p>
  * 
- * @author thies
+ * @author thies & schmidt
  * @version 1.0
  */
 
@@ -29,96 +25,111 @@ public abstract class BusinessObject implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	
 	/**
 	 * Eindeutige ID alles BOs samt getter und setter.
 	 */
 	protected int id = 0;
 
-
-	
 	public int getId() {
 		return this.id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	/**
-	 * @author thies 
-	 * Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz.
-	 * Dies kann selbstverständlich in Subklassen überschrieben werden.
+	 * @author thies Erzeugen einer einfachen textuellen Darstellung der
+	 *         jeweiligen Instanz. Dies kann selbstverständlich in Subklassen
+	 *         überschrieben werden.
 	 */
 	@Override
 	public String toString() {
-		/*
+
+		/**
 		 * Wir geben den Klassennamen gefolgt von der ID des Objekts zurück.
+		 * 
+		 * @return Name und Id
 		 */
 		return this.getClass().getName() + " #" + this.id;
 	}
-	
-	  /**
-	   * <p>
-	   * @author thies 
-	   * Feststellen der <em>inhaltlichen</em> Gleichheit zweier
-	   * <code>BusinessObject</code>-Objekte. Die Gleichheit wird in diesem Beispiel auf eine
-	   * identische ID beschränkt.
-	   * </p>
-	   * <p>
-	   * <b>ACHTUNG:</b> Die inhaltliche Gleichheit nicht mit dem Vergleich der
-	   * <em>Identität</em> eines Objekts mit einem anderen verwechseln!!! Dies
-	   * würde durch den Operator <code>==</code> bestimmt. Bei Unklarheit hierzu
-	   * können Sie nocheinmal in die Definition des Sprachkerns von Java schauen.
-	   * Die Methode <code>equals(...)</code> ist für jeden Referenzdatentyp
-	   * definiert, da sie bereits in der Klasse <code>Object</code> in einfachster
-	   * Form realisiert ist. Dort ist sie allerdings auf die simple Bestimmung der
-	   * Gleicheit der Java-internen Objekt-ID der verglichenen Objekte beschränkt.
-	   * In unseren eigenen Klassen können wir diese Methode überschreiben und ihr
-	   * mehr Intelligenz verleihen.
-	   * </p>
-	   */
-	//TODO: was heisst dieses @override 
-	  @Override
-	public boolean equals(Object o) {
-	    /*
-	     * Abfragen, ob ein Objekt ungleich NULL ist und ob ein Objekt gecastet
-	     * werden kann, sind immer wichtig!
-	     */
-	    if (o != null && o instanceof BusinessObject) {
-	      BusinessObject bo = (BusinessObject) o;
-	      try {
-	        if (bo.getId() == this.id)
-	          return true;
-	      }
-	      catch (IllegalArgumentException e) {
-	        /*
-	         * Wenn irgendetwas schief geht, dann geben wir sicherheitshalber false
-	         * zurück.
-	         */
-	        return false;
-	      }
-	    }
-	    /*
-	     * Wenn bislang keine Gleichheit bestimmt werden konnte, dann müssen
-	     * schließlich false zurückgeben.
-	     */
-	    return false;
-	  }
 
-	  /**
-	   *@author thies 
-	   * <p>
-	   * Erzeugen einer ganzen Zahl, die für das <code>BusinessObject</code> charakteristisch ist.
-	   * </p>
-	   * <p>
-	   * Zusammen mit <code>equals</code> sollte diese Methode immer definiert werden. Manche Java-Klassen
-	   * verwendenden <code>hashCode</code>, um initial ein Objekt (z.B. in einer Hashtable) zu identifizieren. Erst danach
-	   * würde mit <code>equals</code> festgestellt, ob es sich tatsächlich um das gesuchte Objekt handelt.
-	   */
-	  @Override
+	/**
+	 * <p>
+	 * 
+	 * @author thies Feststellen der <em>inhaltlichen</em> Gleichheit zweier
+	 *         <code>BusinessObject</code>-Objekte. Die Gleichheit wird in
+	 *         diesem Beispiel auf eine identische ID beschränkt.
+	 *         </p>
+	 *         <p>
+	 *         <b>ACHTUNG:</b> Die inhaltliche Gleichheit nicht mit dem
+	 *         Vergleich der <em>Identität</em> eines Objekts mit einem anderen
+	 *         verwechseln!!! Dies würde durch den Operator <code>==</code>
+	 *         bestimmt. Bei Unklarheit hierzu können Sie nocheinmal in die
+	 *         Definition des Sprachkerns von Java schauen. Die Methode
+	 *         <code>equals(...)</code> ist für jeden Referenzdatentyp
+	 *         definiert, da sie bereits in der Klasse <code>Object</code> in
+	 *         einfachster Form realisiert ist. Dort ist sie allerdings auf die
+	 *         simple Bestimmung der Gleicheit der Java-internen Objekt-ID der
+	 *         verglichenen Objekte beschränkt. In unseren eigenen Klassen
+	 *         können wir diese Methode überschreiben und ihr mehr Intelligenz
+	 *         verleihen.
+	 *         </p>
+	 */
+	
+	/** (@override)- Mit diesem Typ kann eine Methode gekennzeichnet werden, 
+	 * die die Methode ihrer Oberklasse überschreibt. Der Compiler stellt dann 
+	 * sicher, dass die  Oberklasse diese Methode enthält und gibt einen Fehler 
+	 * aus, wenn dies nicht der Fall ist. 
+	 */
+
+	@Override
+	public boolean equals(Object o) {
+
+		/**
+		 * Abfragen, ob ein Objekt ungleich NULL ist und ob ein Objekt gecastet
+		 * werden kann, sind immer wichtig!
+		 */
+		if (o != null && o instanceof BusinessObject) {
+			BusinessObject bo = (BusinessObject) o;
+			try {
+				if (bo.getId() == this.id)
+					return true;
+			} catch (IllegalArgumentException e) {
+				
+				
+			/**
+			* Wenn irgendetwas schief geht, dann geben wir
+			* sicherheitshalber false zurück.
+			*/
+				return false;
+			}
+		}
+
+		/**
+		 * Wenn bislang keine Gleichheit bestimmt werden konnte, dann müssen
+		 * schließlich false zurückgeben.
+		 */
+		return false;
+	}
+
+	/**
+	 * @author thies
+	 *         <p>
+	 *         Erzeugen einer ganzen Zahl, die für das
+	 *         <code>BusinessObject</code> charakteristisch ist.
+	 *         </p>
+	 *         <p>
+	 *         Zusammen mit <code>equals</code> sollte diese Methode immer
+	 *         definiert werden. Manche Java-Klassen verwendenden
+	 *         <code>hashCode</code>, um initial ein Objekt (z.B. in einer
+	 *         Hashtable) zu identifizieren. Erst danach würde mit
+	 *         <code>equals</code> festgestellt, ob es sich tatsächlich um das
+	 *         gesuchte Objekt handelt.
+	 */
+	@Override
 	public int hashCode() {
-		  return this.id;
-	  }
-	  
+		return this.id;
+	}
+
 }
