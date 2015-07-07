4e18b1e5194ffbfc4c508e83e3b93dcd0285f90b
alexanderPressler <ap061@hdm-stuttgart.de>
2015-07-07 18:16:11 +0200

    Kommentare Exception Klassen

--- hdm-sms/src/de/hdm/gruppe1/shared/DuplicateEnderzeugnisException.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/DuplicateEnderzeugnisException.java
@@ -2,11 +2,18 @@ package de.hdm.gruppe1.shared;
 
 import de.hdm.gruppe1.shared.bo.Enderzeugnis;
 
+/**
+ * Das System hat individuelle Exception-Klassen, 
+ * um die Fehler bei RPC aufrufen zu Klassifizeiren
+ * und die Ursache zu ermitteln
+ * analog zu {@SmsException}  
+ * Diese Exception prüft ob der Name eines Enderzeugnisses bereits 
+ * existiert, und verhindert sonst ein Anlegen der Stueckliste
+ * 
+ * @author Andreas Herrmann
+ */
 public class DuplicateEnderzeugnisException extends SmsException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public DuplicateEnderzeugnisException (){
