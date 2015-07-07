3b9e81e493c5d1f4e9913f7932e548e55cadae28
alexanderPressler <ap061@hdm-stuttgart.de>
2015-07-07 16:22:38 +0200

    Kommentare Applikationsschicht (Galina&Alex)

--- hdm-sms/src/de/hdm/gruppe1/shared/report/LoginService.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/report/LoginService.java
@@ -3,7 +3,11 @@ package de.hdm.gruppe1.shared.report;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-
+/**
+ * Synchrones Interface für den Login service im Report
+ * 
+ * @author Thies, Schmidt & Pressler
+ */
 @RemoteServiceRelativePath("login")
 	public interface LoginService extends RemoteService {
 
