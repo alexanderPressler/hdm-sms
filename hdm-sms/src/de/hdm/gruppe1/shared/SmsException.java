b5e9cd659c2b5f49e9f7ac62054e206f272a7742
alexanderPressler <ap061@hdm-stuttgart.de>
2015-07-07 18:08:55 +0200

    Kommentare Exception Klassen

--- hdm-sms/src/de/hdm/gruppe1/shared/SmsException.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/SmsException.java
@@ -6,28 +6,56 @@ package de.hdm.gruppe1.shared;
 import de.hdm.gruppe1.shared.bo.Element;
 
 /**
+ * Das System hat individuelle Exception-Klassen, 
+ * um die Fehler bei RPC aufrufen zu Klassifizeiren
+ * und die Ursache zu ermitteln
+ * Hierzu besitzten die Exception eine Error Message (String)
+ * und ein Auslöser-Element (Element), dass den Fehler verursacht  
  * @author Andreas Herrmann
  *
  */
 public class SmsException extends Exception {
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Deklaration der Attribute
+	 * message
+	 * Element
+	 * + Setter & Getter
+	 */
 	private String message;
 	private Element ausloeser;
+	
+	/**
+	 * leerer Konstruktor
+	 * @see #SmsException ()
+	 */
 	public SmsException (){
-		
 	}
+	
+	/**
+	 * Konstruktor mit Fehler message
+	 * @see #SmsException (String message)
+	 */
 	public SmsException (String message){
 	setMessage(message);
 	}
 	
+	/**
+	 * Konstruktor mit Auslöser Element
+	 * @see #SmsException (Element cause)
+	 */
 	public SmsException (Element cause){
 		this.ausloeser=cause;
 	}
 	
+	/**
+	 * Konstruktor mit Auslöser Element
+	 * und Fehler message 
+	 * @see #SmsException (String message, Element cause)
+	 */
 	public SmsException (String message, Element cause){
 		this.message=message;
 		this.ausloeser=cause;
