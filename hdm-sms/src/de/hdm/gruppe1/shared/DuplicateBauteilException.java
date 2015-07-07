f8dc25aba5ae4afce92561ea1c7d5813dd62a943
alexanderPressler <ap061@hdm-stuttgart.de>
2015-07-07 18:18:32 +0200

    Update DuplicateBauteilException.java

--- hdm-sms/src/de/hdm/gruppe1/shared/DuplicateBauteilException.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/DuplicateBauteilException.java
@@ -2,11 +2,18 @@ package de.hdm.gruppe1.shared;
 
 import de.hdm.gruppe1.shared.bo.Bauteil;
 
+/**
+ * Das System hat individuelle Exception-Klassen, 
+ * um die Fehler bei RPC aufrufen zu Klassifizeiren
+ * und die Ursache zu ermitteln
+ * analog zu {@SmsException}  
+ * Diese Exception prüft ob der Name eines Bauteils bereits 
+ * existiert, und verhindert sonst ein Anlegen des Bauteils
+ * 
+ * @author Andreas Herrmann
+ */
 public class DuplicateBauteilException extends SmsException {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public DuplicateBauteilException (){
