5a9ec63aaaff2081adbded16d084374d15c1fc6c
alexanderPressler <ap061@hdm-stuttgart.de>
2015-07-07 18:19:54 +0200

    Kommentare Exception Klassen

--- hdm-sms/src/de/hdm/gruppe1/shared/DuplicateBaugruppeException.java
+++ hdm-sms/src/de/hdm/gruppe1/shared/DuplicateBaugruppeException.java
@@ -2,11 +2,18 @@ package de.hdm.gruppe1.shared;
 
 import de.hdm.gruppe1.shared.bo.Baugruppe;
 
+/**
+ * Das System hat individuelle Exception-Klassen, 
+ * um die Fehler bei RPC aufrufen zu Klassifizeiren
+ * und die Ursache zu ermitteln
+ * analog zu {@SmsException}  
+ * Diese Exception prüft ob der Name einer Baugruppe bereits 
+ * existiert, und verhindert sonst ein Anlegen der Baugruppe
+ * 
+ * @author Andreas Herrmann
+ */
 public class DuplicateBaugruppeException extends SmsException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public DuplicateBaugruppeException (){
